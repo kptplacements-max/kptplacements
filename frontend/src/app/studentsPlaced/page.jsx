254c373d28f0264751101b20b907b3f796bc9302
kptudyogamela <kptudyogamela@gmail.com>
2026-05-20 10:21:45 +0530

    updated colorful placed students

--- frontend/src/app/studentsPlaced/page.jsx
+++ frontend/src/app/studentsPlaced/page.jsx
@@ -177,7 +177,7 @@ export default function PlacedStudents() {
           No students match your filters.
         </p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {filtered.map((s) => {
             return (
               <div
